8981d35df3b76a1ea27ee7fd9c6bf60da89d08c6
Fatmanur Özçetin <93328444+Nurrozcetin@users.noreply.github.com>
2024-04-20 12:31:58 +0300

    add images

--- core/src/com/example/survivorbird/SurvivorBird.java
+++ core/src/com/example/survivorbird/SurvivorBird.java
@@ -1,31 +1,40 @@
 package com.example.survivorbird;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class SurvivorBird extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	SpriteBatch batch; //objeler
+	Texture background;
+	Texture bird;
+	float birdX = 0;
+	float birdY = 0;
 	
 	@Override
-	public void create () {
+	public void create () { //oyun acildiginda
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("background.png");
+		bird = new Texture("bird.png");
+
+		birdX = Gdx.graphics.getWidth()/6;
+		birdY = Gdx.graphics.getHeight()/3;
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+	public void render () { //oyun devam ettigi surece devamli cagrilan surekli olmasini istedigimz seyler
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(bird, birdX, birdY,Gdx.graphics.getWidth()/15, Gdx.graphics.getHeight()/10);
+
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
